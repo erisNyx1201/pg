08009fafe135b55b41f4d9913d4c3341929a52b9
erisNyx1201 <97796246+erisNyx1201@users.noreply.github.com>
2022-02-20 22:58:11 +0800

    Add files via upload

---
+++ model/student.dart
@@ -0,0 +1,19 @@
+class Student{
+  final String imagePath;
+  final String name;
+  final String email;
+  final String noMatric;
+  final bool isDarkMode;
+  final String about;
+
+//constructer
+  const Student({
+    required this.imagePath,
+    required this.name,
+    required this.email,
+    required this.noMatric,
+    required this.isDarkMode,
+    required this.about,
+
+  });
+}
